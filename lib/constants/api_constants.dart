0074d1d78b19a0a0716a26022c131d74d95efef1
prathapshanmugam3 <prathapshanmugam5@gmail.com>
2026-06-13 13:42:41 +0530

    api url changes

--- lib/constants/api_constants.dart
+++ lib/constants/api_constants.dart
@@ -3,7 +3,10 @@ import 'package:flutter/foundation.dart';
 class ApiConstants {
   // Adaptive Base URL
   static String get baseUrl {
-    return 'https://rewards-backend-zkhh.onrender.com/api';
+    // Production release APK uses the live API; debug/profile builds use the old backend.
+    return kReleaseMode
+        ? 'https://api.rupirewards.xyz/api'
+        : 'https://rewards-backend-zkhh.onrender.com/api';
   }
 
   static String get loginEndpoint => '$baseUrl/users/google-login';
